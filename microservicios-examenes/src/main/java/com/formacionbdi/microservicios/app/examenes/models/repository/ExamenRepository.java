416a893fc638f14b105f3aa4374206c194dc32a0
Antonio Libreros Lopez <librerosfcfm@gmail.com>
2023-03-16 10:27:19 -0600

    Paginación: CrudRepository->PagingAndSortingRepository

--- microservicios-examenes/src/main/java/com/formacionbdi/microservicios/app/examenes/models/repository/ExamenRepository.java
+++ microservicios-examenes/src/main/java/com/formacionbdi/microservicios/app/examenes/models/repository/ExamenRepository.java
@@ -2,11 +2,11 @@ package com.formacionbdi.microservicios.app.examenes.models.repository;
 
 import com.formacionbdi.microservicios.commons.examenes.models.entity.Examen;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface ExamenRepository extends CrudRepository<Examen, Long> {
+public interface ExamenRepository extends PagingAndSortingRepository<Examen, Long> {
 
     @Query("select e from Examen e where e.nombre like %?1%")
     public List<Examen> findByNombre(String term);
